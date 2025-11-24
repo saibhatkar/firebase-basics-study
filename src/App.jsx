de6474ee65c3dc2fd6f956d560966f5517679acc
Sai Bhatkar <saibhatkar999@gmail.com>
2025-11-24 12:09:17 +0530

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        project setup started
+      </div>
+    </>
+  )
+}
+
+export default App
